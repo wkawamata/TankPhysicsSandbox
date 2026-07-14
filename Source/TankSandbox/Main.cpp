543df76e219e42c38842cafa525802d75a768d29
Wataru Kawamata <wkawamata0323@gmail.com>
2026-07-15 03:12:34 +0900

    Add minimal Tank CMake app shell

---
+++ Source/TankSandbox/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Tank Physics Sandbox" << std::endl;
+    std::cout << "Renderer integration: RtPbrSurvey submodule is available at External/RtPbrSurvey." << std::endl;
+
+    return 0;
+}
